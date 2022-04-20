5dbf42c4e65acb5d8178204f8d0f7ca005960fab
Arne Bouillon <arne.bouillon@gmail.com>
2022-04-20 14:33:43 +0200

    Add documentation

--- Obj.m
+++ Obj.m
@@ -1,3 +1,15 @@
+%% Objective function class
+% There are different types of objective function.
+%  - Terminal cost: (1) J_T(y,u) = 1/2*||y(T)-y_T||^2 + gamma/2 * int_0^T(||u||^2)
+%  - Tracking:      (2) J_d(y,u) = 1/2*int_0^T(||y-y_d||^2 + gamma*||u||^2)
+%
+% Parameters:
+%  - type:  The type of objective function
+%  - gamma: The parameter gamma from (1) or (2)
+%  - _:     The third argument is a vector y_T or a function handle y_d,
+%           depending on whether the type is TerminalCost or Tracking,
+%           respectively
+%
 classdef Obj
     properties
         type
--- ObjType.m
+++ ObjType.m
@@ -1,3 +1,6 @@
+%% Objective function type enumeration
+% For documentation, see the `Obj` class.
+%
 classdef ObjType
     enumeration
         Tracking, TerminalCost
